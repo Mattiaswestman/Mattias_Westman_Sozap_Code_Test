809d2577e903450d522bfba5129e930d91d3dfad
mattiaswestman <mattiaswestman@outlook.com>
2019-10-03 19:26:39 +0200

    Day 1 Finished

---
+++ Code Test/Assets/Scripts/Management/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    //public int playerCount = 2;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            enabled = false;
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void IncreasePlayerCount()
+    {
+        Debug.Log("Increase players");
+    }
+
+    public void DecreasePlayerCount()
+    {
+        Debug.Log("Decrease players");
+    }
+}
